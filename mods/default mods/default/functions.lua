60b1468d2e97f35340fc24e8dfb8524afdcad2ab
jordan4ibanez <jordan4ibanez@gmail.com>
2014-09-16 22:52:25 -0400

    Begin 001. Make falling play sound on hitting ground

--- mods/default mods/default/functions.lua
+++ mods/default mods/default/functions.lua
@@ -90,7 +90,114 @@ end
 --
 -- Legacy
 --
+core.register_entity(":__builtin:falling_node", {
+	initial_properties = {
+		physical = true,
+		collide_with_objects = false,
+		collisionbox = {-0.5,-0.5,-0.5, 0.5,0.5,0.5},
+		visual = "wielditem",
+		textures = {},
+		visual_size = {x=0.667, y=0.667},
+	},
 
+	node = {},
+
+	set_node = function(self, node)
+		self.node = node
+		local stack = ItemStack(node.name)
+		local itemtable = stack:to_table()
+		local itemname = nil
+		if itemtable then
+			itemname = stack:to_table().name
+		end
+		local item_texture = nil
+		local item_type = ""
+		if core.registered_items[itemname] then
+			item_texture = core.registered_items[itemname].inventory_image
+			item_type = core.registered_items[itemname].type
+		end
+		prop = {
+			is_visible = true,
+			textures = {node.name},
+		}
+		self.object:set_properties(prop)
+	end,
+
+	get_staticdata = function(self)
+		return self.node.name
+	end,
+
+	on_activate = function(self, staticdata)
+		self.object:set_armor_groups({immortal=1})
+		--self.object:setacceleration({x=0, y=-10, z=0})
+		self:set_node({name=staticdata})
+	end,
+
+	on_step = function(self, dtime)
+		-- Set gravity
+		self.object:setacceleration({x=0, y=-10, z=0})
+		-- Turn to actual sand when collides to ground or just move
+		local pos = self.object:getpos()
+		local bcp = {x=pos.x, y=pos.y-0.7, z=pos.z} -- Position of bottom center point
+		local bcn = core.get_node(bcp)
+		local bcd = core.registered_nodes[bcn.name]
+		-- Note: walkable is in the node definition, not in item groups
+		if not bcd or
+				(bcd.walkable or
+				(core.get_item_group(self.node.name, "float") ~= 0 and
+				bcd.liquidtype ~= "none")) then
+			if bcd and bcd.leveled and
+					bcn.name == self.node.name then
+				local addlevel = self.node.level
+				if addlevel == nil or addlevel <= 0 then
+					addlevel = bcd.leveled
+				end
+				if core.add_node_level(bcp, addlevel) == 0 then
+					self.object:remove()
+					return
+				end
+			elseif bcd and bcd.buildable_to and
+					(core.get_item_group(self.node.name, "float") == 0 or
+					bcd.liquidtype == "none") then
+				core.remove_node(bcp)
+				return
+			end
+			local np = {x=bcp.x, y=bcp.y+1, z=bcp.z}
+			-- Check what's here
+			local n2 = core.get_node(np)
+			-- If it's not air or liquid, remove node and replace it with
+			-- it's drops
+			if n2.name ~= "air" and (not core.registered_nodes[n2.name] or
+					core.registered_nodes[n2.name].liquidtype == "none") then
+				core.remove_node(np)
+				if core.registered_nodes[n2.name].buildable_to == false then
+					-- Add dropped items
+					local drops = core.get_node_drops(n2.name, "")
+					local _, dropped_item
+					for _, dropped_item in ipairs(drops) do
+						core.add_item(np, dropped_item)
+					end
+				end
+				-- Run script hook
+				local _, callback
+				for _, callback in ipairs(core.registered_on_dignodes) do
+					callback(np, n2, nil)
+				end
+			end
+			-- Create node and remove entity
+			core.add_node(np, self.node)
+			self.object:remove()
+			nodeupdate(np)
+			minetest.sound_play("default_place_node", {
+				pos = np,
+				max_hear_distance = 10,
+				gain = 1.0,
+			})			
+		else
+			-- Do nothing
+		end
+	end
+})
 function default.spawn_falling_node(p, nodename)
 	spawn_falling_node(p, nodename)
 end
