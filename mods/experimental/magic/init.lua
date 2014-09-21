74a96d7293a8afd4fa660bdbf1123814caf83623
jordan4ibanez <jordan4ibanez@gmail.com>
2014-09-21 14:54:21 -0400

    Optimize Magic and add in essence gui

--- mods/experimental/magic/init.lua
+++ mods/experimental/magic/init.lua
@@ -1,3 +1,4 @@
+levels = {}
 dofile(minetest.get_modpath("magic").."/functions.lua")
 dofile(minetest.get_modpath("magic").."/ores.lua")
 --[[
@@ -13,6 +14,12 @@ holy essence 700,          baby blue, good
 good/evil bar, 
 
 make evil ores drop evil essence, and good ores, good essence
+
+make magic directory on world create
+
+eventually release this as a standalone mod, able to be dropped into an existing world.
+
+optimize everything eventually
 
 ]]--
 
@@ -64,35 +71,75 @@ minetest.register_entity("magic:essence", {
 		end
 	end,
 })
---give a new player some xp
+
+--[[
 minetest.register_on_newplayer(function(player)
-	file = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_essence", "w")
+	local file = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_essence", "w")
 	file:write("0")
 	file:close()
 end)
+]]--
 
+--give player a gui for level and give a new player some xp
+minetest.register_on_joinplayer(function(player)
+	local name = player:get_player_name()
+	
+	--check for level file and if not exist, then create
+	if io.open(minetest.get_worldpath().."/"..player:get_player_name().."_essence", "r") == nil then
+		local file = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_essence", "w")
+		file:write("0")
+		file:close()
+		levels[name] = {}
+		levels[name].level = 0
+	else 
+	--if exists, then set and check
+		levels[name] = {}
+		local file = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_essence", "r")
+		local essence = file:read("*l")
+		file:close()
+		levels[name].level = tonumber(essence)
+	end
+	player:hud_add({
+		hud_elem_type = "image",
+		position = {x=0.365,y=0.89},
+		size = "",
+		text = "info_background.png",
+		number = 20,
+		alignment = {x=0.5,y=0.5},
+		offset = {x=0, y=0},
+		scale = {x=6.5,y=1},
+	})
+	levels[name].text = player:hud_add({
+		hud_elem_type = "text",
+		position = {x=0.37,y=0.89},
+		size = "",
+		text = "essence: "..tostring(levels[name].level),
+		number = 0,
+		alignment = {x=0.5,y=0.5},
+		offset = {x=0, y=0},
+	})
+end)
+
+local essence_collection_delay = 3
 --Allow people to collect orbs
 minetest.register_globalstep(function(dtime)
 	for _,player in ipairs(minetest.get_connected_players()) do
 		local pos = player:getpos()
+		local name = player:get_player_name()
 		pos.y = pos.y+1.7
 		for _,object in ipairs(minetest.env:get_objects_inside_radius(pos, 0.5)) do
-			if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "magic:essence" and object:get_luaentity().counted == false then
+			if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "magic:essence" and object:get_luaentity().counted == false and object:get_luaentity().timer > essence_collection_delay then
 				object:setvelocity({x=0,y=0,z=0}) 
 				--maybe add back in sound if viable
 				
-				--do this to prevent multiple countings
+				--do this to prevent multiple countings combined with timer
 				object:get_luaentity().counted = true
 				
-				local file = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_essence", "r")
-				local essence = file:read("*l")
-				file:close()
-				
 				local file = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_essence", "w")
-				essence = tonumber(essence) + 1
-				print(essence)
-				file:write(tostring(essence))
+				levels[name].level = levels[name].level + 1
+				file:write(tostring(levels[name].level))
 				file:close()
+				player:hud_change(levels[name].text, "text", "essence: "..tostring(levels[name].level))
 				
 				object:remove()
 				
@@ -100,14 +147,13 @@ minetest.register_globalstep(function(dtime)
 		end
 		local pos1 = pos--entity stack fix
 		for _,object in ipairs(minetest.env:get_objects_inside_radius(pos, 4)) do
-			if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "magic:essence" and object:get_luaentity().timer > 5 then
+			if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "magic:essence" and object:get_luaentity().timer > essence_collection_delay then
 				if object:get_luaentity().collect then
-					pos1.y = pos1.y
 					local pos2 = object:getpos()
 					local vec = {x=pos1.x-pos2.x, y=pos1.y-pos2.y, z=pos1.z-pos2.z}
-					vec.x = vec.x*5
-					vec.y = vec.y*5
-					vec.z = vec.z*5
+					vec.x = vec.x*5.5
+					vec.y = vec.y*5.5
+					vec.z = vec.z*5.5
 					object:setvelocity(vec)
 				end
 			end
